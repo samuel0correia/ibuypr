e471a018de5a470f362b68fc731264529ad271f9
apsoo-iscte <apsoo@iscte-iul.pt>
2022-04-25 00:22:51 +0100

    prototipo side bar adicionado

--- ibuypr/ibuy/urls.py
+++ ibuypr/ibuy/urls.py
@@ -7,20 +7,20 @@ urlpatterns = [
     path('', views.index, name='index'),
 
     # Login, Conta e Perfil
-    path('loginview', views.loginview, name='loginview'),
-    path('login', views.loginpage, name='loginpage'),
-    path('logoutview', views.logoutview, name='logoutview'),
-    path('criarconta', views.criarconta, name='criarconta'),
-    path('minhaconta', views.minhaconta, name='minhaconta'),
-    path('<str:username>', views.perfil, name='perfil'),
+    #path('loginview', views.loginview, name='loginview'),
+    #path('login', views.loginpage, name='loginpage'),
+    #path('logoutview', views.logoutview, name='logoutview'),
+    #path('criarconta', views.criarconta, name='criarconta'),
+    #path('minhaconta', views.minhaconta, name='minhaconta'),
+    #path('<str:username>', views.perfil, name='perfil'),
 
     # Produtos
-    path('criarproduto', views.criarproduto, name='criarproduto'),
-    path('meusprodutos', views.meusprodutos, name='meusprodutos'),
-    path('carrinho', views.carrinho, name='carrinho'),
-    path('<int:produto_id>', views.produto, name='produto'),
+    #path('criarproduto', views.criarproduto, name='criarproduto'),
+    #path('meusprodutos', views.meusprodutos, name='meusprodutos'),
+    #path('carrinho', views.carrinho, name='carrinho'),
+    #path('<int:produto_id>', views.produto, name='produto'),
 
     # Geral
-    path('error', views.error, name='error'),
+    #path('error', views.error, name='error'),
 
 ]
