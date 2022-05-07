fa6ce6d868c3a983cca0a992f77a2f83ce64ad8f
WildPiggie <wildpiggie@gmail.com>
2022-05-07 14:30:08 +0100

    Pequena alteração ao form de criar conta

--- ibuypr/ibuy/forms.py
+++ ibuypr/ibuy/forms.py
@@ -13,12 +13,15 @@ class ProdutoForm(forms.ModelForm):
         model = Produto
         fields = ['nome', 'quantidade', 'preco', 'descricao', 'condicao', 'categoria']
 
+
 class ComprarProdutoForm(forms.Form):
     quantidade = forms.IntegerField()
+
 
 class ContaForm(forms.Form):
     nome = forms.CharField(label='Nome', max_length=100)
     apelido = forms.CharField(label='Apelido', max_length=100)
     username = forms.CharField(label='Username', max_length=100)
     password = forms.CharField(label='Password', widget=forms.PasswordInput())
-    img_user = forms.FileField(label='Imagem de Utilizador')
+    img_user = forms.FileField(label='Imagem de Utilizador',
+                               widget=forms.FileInput(attrs={'accept': 'image/png, image/gif, image/jpeg'}))
