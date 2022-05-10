2bbfb038d75196c677b3ef5443d6937b32ab73bf
obprs-iscte <obprs@iscte-iul.pt>
2022-05-10 23:05:54 +0100

    Styling in process.

---
+++ ibuypr/ibuy/static/ibuy/scripts.js
@@ -0,0 +1,24 @@
+/* Animação da sidebar */
+$(document).ready(function(){
+    $("#menu-btn").click(function(){
+        $("#menu").css({
+            width: '250px'
+        });
+        $(".sidebar").css({
+            marginLeft: '250px'
+        });
+    });
+});
+$(document).ready(function(){
+    $("#close-menu-btn").click(function(){
+        $("#menu").css({
+            width: '0px'
+        });
+        $(".sidebar").css({
+            marginLeft: '0px'
+        });
+    });
+});
+
+/* next... */
+
